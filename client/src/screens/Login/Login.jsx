d4f2d7ecab09d1939b535d75775b8be654d49735
Grady King <grady.king07@gmail.com>
2021-03-09 13:54:39 -0500

    register page working and almost styled

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -5,7 +5,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -102,10 +101,7 @@ export default function LogIn(props) {
             value={password}
             onChange={handleChange}
           />
-          {/* <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          /> */}
+         
           <Button
             type="submit"
             fullWidth
--- client/src/screens/Register/Register.jsx
+++ client/src/screens/Register/Register.jsx
@@ -1,59 +1,199 @@
+import './Register.css'
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link to = '/' color="inherit" >
+        Skillz
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function Register(props) {
   const [formData, setFormData] = useState({
-    username: '',
-    email:'',
-    password: ''
-  })
-  const { username, email, password } = formData;
-  const { handleRegister } = props;
+        username: '',
+        email:'',
+        password: ''
+      })
+      const { username, email, password } = formData;
+      const { handleRegister } = props;
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+          ...prevState,
+          [name]: value
+        }))
+      }
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }))
-  }
+
+  const classes = useStyles();
 
   return (
-    <form onSubmit={(e) => {
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={(e) => {
       e.preventDefault();
       handleRegister(formData);
     }}>
-      <h3>Register</h3>
-      <label>
-        Username:
-        <input
-          type='text'
-          name='username'
-          value={username}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input
-          type='text'
-          name='email'
-          value={email}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type='password'
-          name='password'
-          value={password}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <button>Submit</button>
-    </form>
-  )
+          
+           
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label='username'
+                name="username"
+                autoComplete="username"
+                value={username}
+                onChange={handleChange}
+              />
+            
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="email"
+                name="email"
+            autoComplete="email"
+            value={email}
+                onChange={handleChange}
+              />
+            
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+            autoComplete="current-password"
+            value={password}
+                onChange={handleChange}
+              />
+            
+            
+             
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Register
+          </Button>
+          <div className='login-link'>
+              <Link to='/login' variant="body2">
+                Already have an account? Sign in
+              </Link>
+              </div>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
+
+// export default function Register(props) {
+//   const [formData, setFormData] = useState({
+//     username: '',
+//     email:'',
+//     password: ''
+//   })
+//   const { username, email, password } = formData;
+//   const { handleRegister } = props;
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData(prevState => ({
+//       ...prevState,
+//       [name]: value
+//     }))
+//   }
+
+//   return (
+//     <form onSubmit={(e) => {
+//       e.preventDefault();
+//       handleRegister(formData);
+//     }}>
+//       <h3>Register</h3>
+//       <label>
+//         Username:
+//         <input
+//           type='text'
+//           name='username'
+//           value={username}
+//           onChange={handleChange}
+//         />
+//       </label>
+//       <br />
+//       <label>
+//         Email:
+//         <input
+//           type='text'
+//           name='email'
+//           value={email}
+//           onChange={handleChange}
+//         />
+//       </label>
+//       <br />
+//       <label>
+//         Password:
+//         <input
+//           type='password'
+//           name='password'
+//           value={password}
+//           onChange={handleChange}
+//         />
+//       </label>
+//       <br />
+//       <button>Submit</button>
+//     </form>
+//   )
+// }
