1074ec73ca76342937f3d8478e3199d67621f35d
Grady King <grady.king07@gmail.com>
2021-03-11 02:56:46 -0500

    footer styled and tested all links

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
-import './Footer.css'
+import "./Footer.css";
+import github from "../../assets/github.png";
 
 export default function Footer() {
   return (
     <footer>
-      <div className='links-containers'>
-        
+      <div className="links-containers">
+        <a href="https://github.com/gradyki">
+          <img src={`${github}`} />
+        </a>
       </div>
     </footer>
-  )
+  );
 }
-
-
