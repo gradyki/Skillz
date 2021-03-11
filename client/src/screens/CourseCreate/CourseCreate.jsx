d046031208194ccc3d5621a07678d11e808201c7
Grady King <grady.king07@gmail.com>
2021-03-10 19:45:27 -0500

    Create screen css complete

--- client/src/screens/CourseCreate/CourseCreate.jsx
+++ client/src/screens/CourseCreate/CourseCreate.jsx
@@ -1,7 +1,34 @@
 import "./CourseCreate.css";
 import { useState } from "react";
 import { getAllCourses } from "../../services/courses";
-import CourseDetail from "../CourseDetail/CourseDetail";
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Avatar from '@material-ui/core/Avatar';
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: '#3cb1de'
+  },
+  form: {
+    width: '100%', 
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function FoodCreate(props) {
   const [formData, setFormData] = useState({
@@ -22,19 +49,83 @@ export default function FoodCreate(props) {
     }));
   };
 
+  const classes = useStyles();
+
   return (
+
+<Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <div className={classes.paper}>
+      <Avatar className={classes.avatar}>
+       
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Create Course
+      </Typography>
     <form
       onSubmit={(e) => {
         e.preventDefault();
         handleCreate(formData);
       }}
     >
-      <h3>Create Course</h3>
-      <label>
+    <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="title"
+            label="title"
+            name="title"
+            autoComplete="title"
+            value={title}
+            onChange={handleChange}
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="Info"
+            label="Info"
+            name="info"
+            autoComplete="info"
+            value={info}
+            onChange={handleChange}
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="image"
+            label="image"
+            name="img_url"
+            autoComplete="image"
+            value={img_url}
+            onChange={handleChange}
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="price"
+            label="price"
+            name="price"
+            autoComplete="price"
+            value={price}
+            onChange={handleChange}
+            autoFocus
+          />
+          
+      {/* <label>
         Title:
         <input type="text" name="title" value={title} onChange={handleChange} />
-      </label>
-      <br />
+      </label> */}
+      {/* <br />
       <label>
         info:
         <input type="text" name="info" value={info} onChange={handleChange} />
@@ -58,11 +149,11 @@ export default function FoodCreate(props) {
           value={price}
           onChange={handleChange}
         />
-      </label>
+      </label> */}
 
-      <select defaultValue="default" onChange={handleChange} name="topic_id">
+      <select id='select' defaultValue="default" onChange={handleChange} name="topic_id">
         <option value="default" disabled>
-          -- dummy --value
+          ------ Select Category ------
         </option>
 
         {topics.map((topic) => {
@@ -72,8 +163,19 @@ export default function FoodCreate(props) {
             </option>
           );
         })}
-      </select>
-      <button>Submit</button>
-    </form>
+          </select>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+          Submit
+          </Button>
+      {/* <button>Submit</button> */}
+        </form>
+        </div>
+      </Container>
   );
 }
